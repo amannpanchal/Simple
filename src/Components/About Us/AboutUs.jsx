852fea2990f8b6e19b4ef401f0e2bf9da16e2c97
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-08-07 12:43:05 +0530

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -10,8 +10,8 @@ const AboutUs = () => {
       <div >
         <Typography.Title className='title'>About Us</Typography.Title>
         <Typography.Paragraph style ={{margin : "0 20px "}}>
-Welcome to Khatu Shayam Tours and Travel Agency
-Hello! I am Aakash Panchal, the proud owner of Khatu Shayam Tours and Travel Agency. Our mission is to provide you with the best travel experiences and memorable journeys, tailored to your needs and preferences.
+Welcome to Khatu Shayam Tours and Travel Agency <br/>
+Hello! I am Aakash Panchal,the proud owner of Khatu Shayam Tours and Travel Agency. Our mission is to provide you with the best travel experiences and memorable journeys, tailored to your needs and preferences.
 
 At Khatu Shayam Tours and Travel, we believe in making travel dreams come true. Whether you're planning a solo adventure, a family vacation, or a corporate trip, we've got you covered. Our team of dedicated professionals is committed to delivering exceptional service and ensuring your travel experience is seamless and enjoyable.
         
