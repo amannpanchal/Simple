ea0d1c0b699a3e614f4e92bb64037a55a6d37bc0
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-08-07 12:52:53 +0530

    Update MyFooter.jsx

--- src/Components/Footer/MyFooter.jsx
+++ src/Components/Footer/MyFooter.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './MyFooter.css';
 import Image from '../../assets/Icon/logo-no-background.png';
-import { Footer } from 'antd/es/layout/layout';
+import { Layout } from 'antd';
 import { Typography } from 'antd';
 import { FaWhatsapp } from 'react-icons/fa';
 import { TbPhoneCalling } from 'react-icons/tb';
 import { useNavigate } from 'react-router-dom';
+
+const { Footer } = Layout;
 
 const MyFooter = ({ scrollToSection }) => {
     const navigate = useNavigate();
@@ -15,52 +17,42 @@ const MyFooter = ({ scrollToSection }) => {
     };
 
     const handleWhatsAppClick = () => {
-        window.location.href = 'https://wa.me/7056118281';
+        window.location.href = 'https://wa.me/917056118281';
     };
+
     const handleClick = (itemKey) => {
         scrollToSection(itemKey);
-        
-       
     };
+
     return (
         <Footer className="footer">
-            <img src={Image} className='logoFooter' alt="Footer Logo" />
-            <div className='footerBottom'>
-                <div className='menuList'>
-                    <div className='menuItem'
-                        onClick={() => handleClick('home')}
-                    
-                    >
+            <img src={Image} className="logoFooter" alt="Footer Logo" />
+            <div className="footerBottom">
+                <div className="menuList">
+                    <div className="menuItem" onClick={() => handleClick('home')}>
                         Home
                     </div>
-                    <div className='menuItem'
-                        onClick={() => handleClick('chooseus')}
-                    
-                    >
+                    <div className="menuItem" onClick={() => handleClick('chooseus')}>
                         Routes
                     </div>
-                    <div className='menuItem' 
-                        onClick={() => handleClick('services')}
-                    
-                    >
+                    <div className="menuItem" onClick={() => handleClick('services')}>
                         Services
                     </div>
-                    <div className='menuItem' onClick={() => handleClick('aboutus')}
-                    
-                    
-                    >
+                    <div className="menuItem" onClick={() => handleClick('aboutus')}>
                         About Us
                     </div>
                 </div>
-                <div className='menuList'>
-                    <div className='menuItem center' onClick={handleWhatsAppClick}>
-                        <FaWhatsapp className="icons" />+917056118281
-                    <div className='menuItem center' onClick={handleCallClick}>
-                        <TbPhoneCalling className="icons" /> +917056118281                    </div>
+                <div className="menuList">
+                    <div className="menuItem center" onClick={handleWhatsAppClick}>
+                        <FaWhatsapp className="icons" /> +917056118281
+                    </div>
+                    <div className="menuItem center" onClick={handleCallClick}>
+                        <TbPhoneCalling className="icons" /> +917056118281
+                    </div>
                 </div>
             </div>
         </Footer>
     );
-}
+};
 
 export default MyFooter;
